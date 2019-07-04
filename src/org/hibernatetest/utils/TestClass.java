6fc6fcb9aba4409bd129c93e0beb9575a88a7a19
mohan22 <jakkula4@gmail.com>
2019-07-04 14:40:09 +0530

    HQL pagination

--- src/org/hibernatetest/utils/TestClass.java
+++ src/org/hibernatetest/utils/TestClass.java
@@ -6,20 +6,25 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernatetest.UserDetails;
 
 public class TestClass {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		Query query = session.createQuery("from UserDetails where userId>5");
-		List users = query.list();
+		Query query = session.createQuery("from UserDetails ");
+		query.setFirstResult(5);
+		query.setMaxResults(3);
+		List<UserDetails> users = query.list();
 		// session.getTransaction().commit();
 		session.close();
-		System.out.println("size = " + users.size());
+		for (UserDetails u : users)
+			System.out.println(u.getUserName());
 
 	}
 
