4882c6892e55a5caf715d73c937028bc7cf1d517
mohan22 <jakkula4@gmail.com>
2019-07-04 14:30:00 +0530

    HQL example

--- src/org/hibernatetest/utils/TestClass.java
+++ src/org/hibernatetest/utils/TestClass.java
@@ -1,27 +1,25 @@
 package org.hibernatetest.utils;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernatetest.UserDetails;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 
-		UserDetails user = new UserDetails();
-		user.setUserName("u22");
-		user.setUserName("Updated user name"); // user - Transient object
-
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		session.save(user); // user - persisted object
-
-		session.getTransaction().commit();
-		session.close(); // user- Detached object
-		// System.out.println("User name :" + user.getUserName());
+		Query query = session.createQuery("from UserDetails where userId>5");
+		List users = query.list();
+		// session.getTransaction().commit();
+		session.close();
+		System.out.println("size = " + users.size());
 
 	}
 
